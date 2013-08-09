010de637708625bf10e78634bfee21947e538319
Stéphane Klein <stephaneklein221@gmail.com>
2013-08-09 14:08:47 +0200

    Removed useless spaces

--- Installer.php
+++ Installer.php
@@ -26,7 +26,7 @@ class Installer extends LibraryInstaller
         $properties = $this->resolvePackageName($package->getName());
 
         try {
-            $this->io->write("  -  Installing <info>{$package->getName()}</info> as a Claroline plugin");
+            $this->io->write("  - Installing <info>{$package->getName()}</info> as a Claroline plugin");
             $coreInstaller->install($properties['fqcn'], $properties['path']);
         } catch (\Exception $ex) {
             parent::uninstall($repo, $package);
@@ -46,7 +46,7 @@ class Installer extends LibraryInstaller
     {
         $coreInstaller = $this->getCoreInstaller();
         $properties = $this->resolvePackageName($package->getName());
-        $this->io->write("  -  Uninstalling Claroline plugin <info>{$package->getName()}</info>");
+        $this->io->write("  - Uninstalling Claroline plugin <info>{$package->getName()}</info>");
         $coreInstaller->uninstall($properties['fqcn']);
         parent::uninstall($repo, $package);
     }
